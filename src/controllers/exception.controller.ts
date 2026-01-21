e058994864b0fb1e8f4f8e8c7667dc90f64c0dbc
tuancao <vantuan26072002@gmail.com>
2026-01-22 00:52:08 +0700

    [FEAT] Commit code

--- src/controllers/exception.controller.ts
+++ src/controllers/exception.controller.ts
@@ -3,8 +3,8 @@ import type { Request, Response } from 'express';
 
 export default class ExceptionController {
     constructor() {}
-    public async endpointException(req: Request, res: Response): Promise<Response> {
+    public endpointException = async (req: Request, res: Response): Promise<Response> => {
         const responseError = new ResponseHandler(404, false, 'Endpoint not found', null);
         return res.status(responseError.status).json(responseError);
-    }
+    };
 }
